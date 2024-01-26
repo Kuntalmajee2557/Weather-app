382efe1a73132d8bdaf4ba8233f204fe242d9714
gitcoder <kuntalmajee338@gmail.com>
2024-01-26 20:40:01 +0530

    main part developed

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-async function fetchData() {
-    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '357ce7f9d4msh35229ce532c70c3p139112jsndbacaaa2d98e',
-            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-        }
-    };
 
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-
-        cloud_pct.innerHTML = response.cloud_pct
-        temp.innerHTML = response.temp
-        feels_like.innerHTML = response.feels_like
-        humidity.innerHTML = response.humidity
-        min_temp.innerHTML = response.min_temp
-        max_temp.innerHTML = response.max_temp
-        wind_speed.innerHTML = response.wind_speed
-        wind_degrees.innerHTML = response.wind_degrees
-        sunrise.innerHTML = response.sunrise
-        sunset.innerHTML = response.sunset
-
-    } catch (error) {
-        console.error(error);
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '357ce7f9d4msh35229ce532c70c3p139112jsndbacaaa2d98e',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
     }
+};
+
+const getWeather = (city) => {
+    cityName.innerHTML = city;
+    fetch(url + city, options)
+    .then(response => response.json())
+    .then((response) => {
+        console.log(response);
+        temp.innerHTML = response.temp;
+        temp2.innerHTML = response.temp;
+        feels_like.innerHTML = response.feels_like;
+        humidity.innerHTML = response.humidity;
+        humidity2.innerHTML = response.humidity;
+        min_temp.innerHTML = response.min_temp;
+        max_temp.innerHTML = response.max_temp;
+        wind_speed.innerHTML = response.wind_speed;
+        wind_speed2.innerHTML = response.wind_speed;
+        wind_degrees.innerHTML = response.wind_degrees;
+        sunrise.innerHTML = response.sunrise;
+        sunset.innerHTML = response.sunset
+    })
+    .catch(err => console.error(err));
 }
 
-// Call the async function
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value)
+});
 
-
-fetchData();
+getWeather("Delhi")
